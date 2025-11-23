0a31bfd08800e58561b8452f327dfd104e7bb985
NoFundProject <104980058+ae16z@users.noreply.github.com>
2025-11-23 19:45:32 +0700

    Update shareValidator.js

--- MiningPool/shareValidator.js
+++ MiningPool/shareValidator.js
@@ -1,1 +1,18 @@
+// ---------------------------
+// shareValidator.js
+// ---------------------------
+import { sha256PackedHex, keccakPackedHex } from './utils/hashing.js';
+import { passesTarget } from './utils/difficulty.js';
 
+
+export function validateShare(algo, challenge, miner, nonce, solution, poolTargetHex) {
+// algo: 0=SHA256,1=KECCAK
+let digest;
+if (algo === 0) {
+digest = sha256PackedHex(challenge, miner, BigInt(nonce), BigInt(solution));
+} else {
+digest = keccakPackedHex(challenge, miner, BigInt(nonce), BigInt(solution));
+}
+const ok = passesTarget(digest, poolTargetHex);
+return { ok, digest };
+}
